dec9f687824ba94480bb6e55590350be45ac455b
dcstylexf <dcstylexf@gmail.com>
2025-01-09 17:27:39 +0700

    Update

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -72,8 +72,8 @@ class Article extends Model
     public function getDynamicSEOData(): SEOData
     {
         return new SEOData(
-            title: $this->meta_title && $this->meta_title !== '' ? $this->meta_title : $this->name,
-            description: $this->meta_description && $this->meta_description !== '' ? $this->meta_description : $this->description,
+            title: $this->meta_title && $this->meta_title !== '' ? $this->meta_title : $this->title,
+            description: $this->meta_description && $this->meta_description !== '' ? $this->meta_description : $this->exceprt(),
             image: Storage::url($this->image)
         );
     }
