6418a233b1e66b4514a8fdf52a025bca728ad402
dcstylexf <dcstylexf@gmail.com>
2025-02-21 17:43:50 +0700

    Update

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -117,7 +117,7 @@ class Article extends Model
         if (count($matches) > 0) {
             $img = (isset($matches[0][0])) ? $matches[0][0] : '';
             preg_match('/src="([^"]+)"/', $img, $src);
-            return $src[1];
+            return $src[1] ?? 'https://placeholder.co/300';
         }
 
         // If not, return a default image
