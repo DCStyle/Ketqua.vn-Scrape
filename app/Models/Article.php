c6377b641c722f66bd9503d8d0a5ec89d02ab8db
dcstylexf <dcstylexf@gmail.com>
2025-03-18 16:41:56 +0700

    Update

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -117,7 +117,18 @@ class Article extends Model
         if (count($matches) > 0) {
             $img = (isset($matches[0][0])) ? $matches[0][0] : '';
             preg_match('/src="([^"]+)"/', $img, $src);
-            return $src[1] ?? 'https://placehold.co/300?text=' . urlencode($this->title);
+
+            if (isset($src[1]))
+            {
+                $imageUrl = $src[1];
+                if (Str::startsWith($imageUrl, 'http')) {
+                    return $imageUrl;
+                } else {
+                    return Storage::url($imageUrl);
+                }
+            }
+
+            return 'https://placehold.co/300?text=' . urlencode($this->title);
         }
 
         // If not, return a default image
