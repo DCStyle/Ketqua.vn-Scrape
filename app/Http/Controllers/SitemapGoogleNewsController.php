516b5c8def714ca1f8bf01b815a3153c163b155d
dcstylexf <dcstylexf@gmail.com>
2025-03-18 16:49:24 +0700

    Update

--- app/Http/Controllers/SitemapGoogleNewsController.php
+++ app/Http/Controllers/SitemapGoogleNewsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class SitemapGoogleNewsController extends Controller
 {
@@ -16,7 +17,7 @@ class SitemapGoogleNewsController extends Controller
                 return [
                     'title' => $article->title,
                     'link' => route('articles.show', $article->slug),
-                    'image' => $article->getThumbnail(),
+                    'image' => Str::startsWith($article->image, 'http') ? $article->image : Storage::url($article->image),
                     'published' => \Illuminate\Support\Carbon::parse($article->created_at)
                         ->setTimezone('Asia/Ho_Chi_Minh')
                         ->toW3cString(),
